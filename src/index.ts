ae48be0f4ebb3a5bc9f0a1b8f4eff37250ea4312
Winton DeShong <contact@winton.me>
2020-06-16 10:45:00 -0400

    Migrated stub resource and related record; As well as factories

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 
 export * from "./factories/axios-response-factory";
 export * from "./factories/factory-type";
+export * from "./factories/stub-resource-record-factory";
 
 //#endregion Factories
 
@@ -15,3 +16,12 @@ export * from "./mocks/axios";
 export * from "./mocks/mock-axios";
 
 //#endregion Mocks
+
+// -----------------------------------------------------------------------------------------
+// #region Stubs
+// -----------------------------------------------------------------------------------------
+
+export * from "./stubs/stub-resource";
+export * from "./stubs/stub-resource-record";
+
+//#endregion Stubs
